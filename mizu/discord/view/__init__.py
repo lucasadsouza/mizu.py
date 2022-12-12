573e42b237c8d15ead1d5300168a4ddaa88aa8b3
lucasadsouza <lucasadsouza.dev@gmail.com>
2022-12-11 21:36:10 -0300

    Created ImagePagination to display a list of images and select one (4.0.38)

--- mizu/discord/view/__init__.py
+++ mizu/discord/view/__init__.py
@@ -1,5 +1,5 @@
 from mizu.discord.view.button import Button
 from mizu.discord.view.button import URLButton
 from mizu.discord.view.dropdown import Dropdown
-from mizu.discord.view.pagination import Pagination
+from mizu.discord.view.pagination import Pagination, ImagePagination
 from mizu.discord.view.modal import Modal
--- mizu/discord/view/pagination.py
+++ mizu/discord/view/pagination.py
@@ -1,10 +1,10 @@
-import datetime, math
+import datetime, math, inspect
 import nextcord
 from nextcord.ext import commands
 
 
 class Pagination(nextcord.ui.View):
-  def __init__(self, title: str=None, author: str=None, colour: int=None, type_: str='rich', url: str=None, description: str=None, timestamp: datetime.datetime=None, footer: str=None, image: str=None, thumbnail: str=None, fields_limit: int=5, bot: commands.Bot=None):
+  def __init__(self, title: str=None, author: str=None, colour: int=None, type_: str='rich', url: str=None, description: str=None, timestamp: datetime.datetime=None, footer: str=None, thumbnail: str=None, fields_limit: int=5, bot: commands.Bot=None):
     super().__init__()
     self.value = None
   
@@ -21,9 +21,6 @@ class Pagination(nextcord.ui.View):
 
     if footer:
       self.embed.set_footer(footer)
-    
-    if image:
-      self.set_image(image)
 
     if thumbnail:
       self.set_thumbnail(thumbnail)
@@ -47,10 +44,6 @@ class Pagination(nextcord.ui.View):
       self.embed.add_field(name=name, value=value, inline=inline)
 
     self.total_pages = math.ceil(len(self.fields) / self.fields_limit)
-
-  def add_fields(self, fields: list[dict]):
-    for field in fields:
-      self.add_field(**field)
 
   def turn_page(self):
     self.embed.clear_fields()
@@ -103,3 +96,101 @@ class Pagination(nextcord.ui.View):
     self.turn_page()
     
     await self.embed_msg.edit(embed=self.embed, view=self)
+
+  @nextcord.ui.button(style=nextcord.ButtonStyle.blurple, emoji='<:done:1051614776922484836>')
+  async def select_image(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+    if inspect.iscoroutinefunction(self.callback_func):
+      await self.callback_func(interaction, self.current_page - 1)
+
+    else:
+      self.callback_func(interaction, self.current_page - 1)
+
+
+class ImagePagination(nextcord.ui.View):
+  def __init__(self, title: str=None, author: str=None, colour: int=None, type_: str='rich', url: str=None, description: str=None, timestamp: datetime.datetime=None, footer: str=None, thumbnail: str=None, images_limit: int=5, bot: commands.Bot=None, callback_func: callable=None):
+    super().__init__()
+    self.value = None
+  
+    self.bot = bot
+    self.embed_msg = None
+    self.images = []
+    self.current_page = 1
+    self.total_pages = 1
+    self.images_limit = images_limit
+    self.callback_func = callback_func
+
+    self.embed = self.create_embed(title, colour, type_, url, description, timestamp)
+
+    if author:
+      self.embed.set_author(author)
+
+    if footer:
+      self.embed.set_footer(footer)
+
+    if thumbnail:
+      self.set_thumbnail(thumbnail)
+
+  def create_embed(self, title: str, colour: int, type_: str='rich', url: str=None, description: str=None, timestamp: datetime.datetime=None):
+    embed = nextcord.Embed(colour=colour, title=title, type=type_, url=url, description=description, timestamp=timestamp)
+
+    return embed
+
+  def add_image(self, url: str):
+    self.images.append(url)
+
+    if len(self.images) == 1:
+      self.embed.set_image(url)
+
+    self.total_pages = len(self.images)
+
+  def turn_page(self):
+    self.embed.set_image(self.images[self.current_page - 1])
+
+  async def send(self, interaction: nextcord.Interaction):
+    self.children[1].label = f'{self.current_page}/{self.total_pages}'
+
+    self.embed_msg = await interaction.send(embed=self.embed, view=self)
+
+  @nextcord.ui.button(emoji='<:arrow_back:1046879600703189032>', disabled=True)
+  async def preview_page(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+    self.current_page -= 1
+
+    if self.current_page == 1:
+      self.children[0].disabled = True
+
+    if self.current_page < self.total_pages:
+      self.children[2].disabled = False
+
+    self.children[1].label = f'{self.current_page}/{self.total_pages}'
+    
+    self.turn_page()
+
+    await self.embed_msg.edit(embed=self.embed, view=self)
+
+  @nextcord.ui.button(label='0/1', disabled=True)
+  async def page_label(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+    pass
+
+  @nextcord.ui.button(emoji='<:arrow_forward:1046879585297510481>')
+  async def next_page(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+    self.current_page += 1
+
+    if self.current_page > 1:
+      self.children[0].disabled = False
+
+    if self.current_page == self.total_pages:
+      self.children[2].disabled = True
+
+    self.children[1].label = f'{self.current_page}/{self.total_pages}'
+
+    self.turn_page()
+    
+    await self.embed_msg.edit(embed=self.embed, view=self)
+
+  @nextcord.ui.button(style=nextcord.ButtonStyle.blurple, emoji='<:done:1051614776922484836>')
+  async def select_image(self, button: nextcord.ui.Button, interaction: nextcord.Interaction):
+    if inspect.iscoroutinefunction(self.callback_func):
+      await self.callback_func(interaction, self.current_page - 1)
+
+    else:
+      self.callback_func(interaction, self.current_page - 1)
